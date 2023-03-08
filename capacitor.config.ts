229a35f617cbde6f42b9f1db69a8e7061f5acc64
Damian Tarnawsky <damian@ionic.io>
2023-03-08 08:59:23 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.v7.toggle',
+  appName: 'cs-v7-toggle',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
